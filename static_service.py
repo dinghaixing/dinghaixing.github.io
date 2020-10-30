f381165e4730f5001705e2ae4c910c4568bfea0f
Dinglish <87053970@qq.com>
2020-10-30 16:41:48 +0800

    change service port

--- static_service.py
+++ static_service.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	
 	cherrypy.config.update({
 		'server.socket_host': '0.0.0.0',
-		'server.socket_port': 801,
+		'server.socket_port': 80,
 		'log.screen': False
 	})
 
